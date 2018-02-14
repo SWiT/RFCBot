8dadf8e723bfc7607b8fe819954082510257b714
Matthew Switlik <switlikm@gmail.com>
2018-02-13 22:07:18 -0500

    driving! needs work though.

--- drive.py
+++ drive.py
@@ -6,11 +6,11 @@ import pygame
 servoleft = 18
 servoright = 13
 
-leftforward = 50
-rightforward = 200
+leftforward = 200
+rightforward = 50
 stop = 150
-leftreverse = 200
-rightreverse = 50
+leftreverse = 50
+rightreverse = 200
 
 # use 'GPIO naming'
 wiringpi.wiringPiSetupGpio()
@@ -47,17 +47,52 @@ while run:
     for event in pygame.event.get():
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
         if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-            for b in range(joystick.get_numbuttons())
-                print b,joystick.get_button(b)
+            #print("Joystick button pressed.")
+            for b in range(joystick.get_numbuttons()):
+                if joystick.get_button(b) and b%2==0:
+                    print "Faster"
+                if joystick.get_button(b) and b%2==1:
+                    print "Slower"
         if event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
         if event.type == pygame.JOYAXISMOTION:
-            print("JOYAXISMOTION")    
-        
-    #wiringpi.pwmWrite(stop, 150)
-    #wiringpi.pwmWrite(stop, 150)
-    #time.sleep(delay_period)
+            x = joystick.get_axis(0)
+            y = joystick.get_axis(1)
+            if x == 0 and y == 0:
+                print("Stop")
+                wiringpi.pwmWrite(servoleft, stop)
+                wiringpi.pwmWrite(servoright, stop)
+                
+            elif x == 0 and y > 0:
+                print("Forward")
+                wiringpi.pwmWrite(servoleft, leftforward)
+                wiringpi.pwmWrite(servoright, rightforward)
+                
+            elif x == 0 and y < 0:
+                print("Reverse")
+                wiringpi.pwmWrite(servoleft, leftreverse)
+                wiringpi.pwmWrite(servoright, rightreverse)
+                
+            elif x < 0 and y == 0:
+                print("Spin left")
+                wiringpi.pwmWrite(servoleft, leftreverse)
+                wiringpi.pwmWrite(servoright, rightforward)
+            elif x > 0 and y == 0:
+                print("Spin right")
+                wiringpi.pwmWrite(servoleft, leftforward)
+                wiringpi.pwmWrite(servoright, rightreverse)
+                
+            elif x < 0 and y > 0:
+                print("Forward left")
+            elif x > 0 and y > 0:
+                print("Forward right")
+                
+            elif x < 0 and y < 0:
+                print("Reverse left")
+            elif x > 0 and y < 0:
+                print("Reverse right")
+                
+    time.sleep(delay_period)
 
 pygame.joystick.quit()
 print "exit."

eb153e26bab0f4ef86492494a1e45a1eb2089174
Matthew G. Switlik <switlikm@gmail.com>
2018-02-13 19:51:32 -0500

    wiimote

--- attachwii.sh
+++ attachwii.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-#exit 1
-
 sleep 1 # Wait until Bluetooth services are fully initialized
 hcitool dev | grep hci >/dev/null
 if test $? -eq 0 ; then
-    	echo "Launching wminput"
-	wminput -d -c  /home/pi/RFCBot/mywminput 00:17:AB:29:52:2A > /dev/null 2>&1 &
+    echo "Launching wminput"
+    wminput -d -c  /home/pi/RFCBot/mywminput 00:17:AB:29:52:2A > /dev/null 2>&1 &
 else
-	echo "Blue-tooth adapter not present!"
-	exit 1
+    echo "Blue-tooth adapter not present!"
+    exit 1
 fi
 

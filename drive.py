0ec1f5809f69a1e88c026041334b3ed0f3f0a100
Matthew Switlik <switlikm@gmail.com>
2020-08-27 20:42:21 -0400

    rfcbot class

--- drive.py
+++ drive.py
@@ -5,130 +5,103 @@ import time
 import wiringpi
 import pygame
 
-servoleft = 18
-servoright = 13
+import rfcbot
 
-leftforward = 50
-leftstop = 148
-leftreverse = -50
+bot = rfcbot.RFCBot()
 
-rightforward = -50
-rightstop = 147
-rightreverse = 50
+drivehat = 0        # The joystick hat used to drive.
+driveaxislr = 0     # The Left and Right joystick axis.
+driveaxisfb = 1     # The Forwards and Backwards joystick axis.
 
+waiting_period = 1.0
 delay_period = 0.01
-throttle = 0.40
-accel = 0.05
-
-def calcSpeed(direction, stop, throttle):
-    return int((direction * throttle) + stop)
 
 
-# use 'GPIO naming'
-wiringpi.wiringPiSetupGpio()
-
-# set #18 to be a PWM output
-wiringpi.pinMode(servoleft, wiringpi.GPIO.PWM_OUTPUT)
-wiringpi.pinMode(servoright, wiringpi.GPIO.PWM_OUTPUT)
-
-# set the PWM mode to milliseconds stype
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-
-# divide down clock
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
-
-run = True
-
+# Initialize Pygame
 pygame.init()
+
+# Initialize any joysticks
 pygame.joystick.init()
-
-
+# If no joystick then wait.
 joystickcount = pygame.joystick.get_count()
-print("RFC: "+str(joystickcount)+" Joysticks")
 if joystickcount < 1:
     print "RFC: Waiting for Joystick..."
 while joystickcount < 1:
-    time.sleep(delay_period*100)
     pygame.joystick.quit()
+    time.sleep(waiting_period)
     pygame.joystick.init()
     joystickcount = pygame.joystick.get_count()
-    
+
+# Connect to the first joystick.    
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 print("RFC: Joystick(0) "+str(joystick.get_numbuttons())+" Buttons")
-    
 
+
+def drive(x, y):
+    if x == 0 and y == 0:
+        bot.stop()
+        
+    elif x == 0 and y > 0:
+        bot.forward()
+        
+    elif x == 0 and y < 0:
+        bot.reverse()
+        
+    elif x < 0 and y == 0:
+        bot.spinleft()
+    
+    elif x > 0 and y == 0:
+        bot.spinright()
+        
+    elif x < 0 and y > 0:
+        bot.turnforwardleft()
+        
+    elif x > 0 and y > 0:
+        bot.turnforwardright()
+        
+    elif x < 0 and y < 0:
+        bot.turnreverseleft()
+    
+    elif x > 0 and y < 0:
+        bot.turnreverseright()
+    
+    
+run = True
 while run:
     for event in pygame.event.get():
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        # Button Down
         if event.type == pygame.JOYBUTTONDOWN:
-            #print("Joystick button pressed.")
             for b in range(joystick.get_numbuttons()):
-                if joystick.get_button(b) and b%2==0:
-                    throttle = throttle + accel
-                    if throttle > 1:
-                        throttle = 1
-                    print "RFC: Faster", throttle
+                if joystick.get_button(b):
+                    print "RFC: Button", b, "Down"
                     
-                if joystick.get_button(b) and b%2==1:
-                    throttle = throttle - accel
-                    if throttle < 0:
-                        throttle = 0
-                    print "RFC: Slower", throttle
-                        
+        # Button Up                
         if event.type == pygame.JOYBUTTONUP:
-            #print("Joystick button released.")
-            pass
-            
+            for b in range(joystick.get_numbuttons()):
+                if joystick.get_button(b):
+                    print "RFC: Button", b, "Down"
+                    
+        # Hat Motion
+        if event.type == pygame.JOYHATMOTION:
+            for h in range(joystick.get_numhats()):
+                hat = joystick.get_hat(h)
+                print "RFC: HAT ", h, hat
+                x = hat[0]
+                y = hat[1]
+                drive(x, y)
+                
+        # Axis Motion    
         if event.type == pygame.JOYAXISMOTION:
-            x = joystick.get_axis(0)
-            y = joystick.get_axis(1)
+            axes = ""
+            for a in range(joystick.get_numaxes()):
+                axes += str(joystick.get_axis(a)) + " "
+            print "RFC: AXES ", axes
+            x = joystick.get_axis(driveaxislr)
+            y = -1 * joystick.get_axis(driveaxisfb)
+            drive(x, y)
             
-            if x == 0 and y == 0:
-                print("RFC: Stop")
-                wiringpi.pwmWrite(servoleft, leftstop)
-                wiringpi.pwmWrite(servoright, rightstop)
-                
-            elif x == 0 and y > 0:
-                print "RFC: Forward", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftforward, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightforward, rightstop, throttle))
-                
-            elif x == 0 and y < 0:
-                print "RFC: Reverse", throttle 
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftreverse, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightreverse, rightstop, throttle))
-                
-            elif x < 0 and y == 0:
-                print "RFC: Spin left", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftreverse, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightforward, rightstop, throttle))
-            
-            elif x > 0 and y == 0:
-                print "RFC: Spin right", throttle 
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftforward, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightreverse, rightstop, throttle))
-                
-            elif x < 0 and y > 0:
-                print "RFC: Forward left", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftforward, leftstop, throttle/2))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightforward, rightstop, throttle))
-                
-            elif x > 0 and y > 0:
-                print "RFC: Forward right", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftforward, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightforward, rightstop, throttle/2))
-                
-            elif x < 0 and y < 0:
-                print "RFC: Reverse left", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftreverse, leftstop, throttle/2))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightreverse, rightstop, throttle))
-            
-            elif x > 0 and y < 0:
-                print "RFC: Reverse right", throttle
-                wiringpi.pwmWrite(servoleft, calcSpeed(leftreverse, leftstop, throttle))
-                wiringpi.pwmWrite(servoright, calcSpeed(rightreverse, rightstop, throttle/2))
                 
     time.sleep(delay_period)
 
---
+++ rfcbot.py
@@ -0,0 +1,82 @@
+import wiringpi
+
+class RFCBot:
+    # Class for interacting with the Robot Fight Club RPi0W Bot
+    
+    servoleft = 18      # Left servo GPIO pin
+    servoright = 13     # Right servo GPIO pin
+
+    LF = 50     # Max offset for the left servos forward direction
+    LS = 148    # The left servos stopped position
+    LR = -50    # Max offset for the left servos reverse direction
+
+    RF = -50
+    RS = 147
+    RR = 50
+
+    throttle = 1.0
+    accel = 0.05
+    
+    def __init__(self):
+        # use 'GPIO naming'
+        wiringpi.wiringPiSetupGpio()
+
+        # set #18 to be a PWM output
+        wiringpi.pinMode(self.servoleft, wiringpi.GPIO.PWM_OUTPUT)
+        wiringpi.pinMode(self.servoright, wiringpi.GPIO.PWM_OUTPUT)
+
+        # set the PWM mode to milliseconds stype
+        wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+        # divide down clock
+        wiringpi.pwmSetClock(192)
+        wiringpi.pwmSetRange(2000)
+
+    def calcPWM(self, direction, stop, throttle):
+        return int((direction * throttle) + stop)
+    
+    def stop(self):
+        print("RFC: Stop")
+        wiringpi.pwmWrite(self.servoleft, self.LS)
+        wiringpi.pwmWrite(self.servoright, self.RS)
+        
+    def forward(self):
+        print "RFC: Forward"
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LF, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RF, self.RS, self.throttle))
+        
+    def reverse(self):
+        print "RFC: Reverse", self.throttle 
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LR, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RR, self.RS, self.throttle))
+    
+    def spinleft(self):
+        print "RFC: Spin left", self.throttle
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LR, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RF, self.RS, self.throttle))
+            
+    def spinright(self):
+        print "RFC: Spin right", self.throttle 
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LF, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RR, self.RS, self.throttle))
+                
+    def turnforwardleft(self):
+        print "RFC: Turn forward left", self.throttle
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LF, self.LS, self.throttle/2))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RF, self.RS, self.throttle))
+    
+    def turnforwardright(self):
+        print "RFC: Turn forward right", self.throttle
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LF, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RF, self.RS, self.throttle/2))
+                
+    def turnreverseleft(self):
+        print "RFC: Turn reverse left", self.throttle
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LR, self.LS, self.throttle/2))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RR, self.RS, self.throttle))
+            
+    def turnreverseright(self):
+        print "RFC: Turn reverse right", self.throttle
+        wiringpi.pwmWrite(self.servoleft, self.calcPWM(self.LR, self.LS, self.throttle))
+        wiringpi.pwmWrite(self.servoright, self.calcPWM(self.RR, self.RS, self.throttle/2))    
+        
